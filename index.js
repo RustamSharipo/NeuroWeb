d976cbc30944d460a8ab63131a65dddd36b667db
Rustam <102970338+RustamSharipo@users.noreply.github.com>
2025-05-03 19:59:26 +0500

    first

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const arrow = document.getElementById('arrow');
+const imgBlock = document.getElementById('ImgBlock');
 
     arrow.addEventListener('click', () => {
       arrow.classList.toggle('rotated');
 });
+arrow.addEventListener('click', () => {
+  if (imgBlock.style.display === 'block') {
+    imgBlock.style.display = 'none';
+  } else {
+    imgBlock.style.display = 'block';
+  }
+});
+
+
