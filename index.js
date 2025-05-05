c8e4acc4d349ce44473bdef0a1a1c65a2641b788
Rustam <102970338+RustamSharipo@users.noreply.github.com>
2025-05-05 17:36:58 +0500

    first

--- index.js
+++ index.js
@@ -17,3 +17,21 @@ function toggleBlock(){
     }, 10); 
   }
 }
+const arrow2 = document.getElementById('arrow2');
+arrow2.addEventListener('click', () => {
+  arrow2.classList.toggle('rotated');
+});
+function toggleBlock2(){
+  const imgBlock2 = document.getElementById('ImgBlock2');
+  if (imgBlock2.classList.contains('visible')) {
+    imgBlock2.classList.remove('fade_in');
+    setTimeout(() => {
+      imgBlock2.classList.remove('visible');
+    }, 500);
+  } else {
+    imgBlock2.classList.add('visible');
+    setTimeout(() => {
+      imgBlock2.classList.add('fade_in');
+    }, 10); 
+  }
+}
