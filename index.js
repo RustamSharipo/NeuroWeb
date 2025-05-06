afbe9bd4055d8fa20c2257604e9124943102919e
Rustam <102970338+RustamSharipo@users.noreply.github.com>
2025-05-06 16:45:57 +0500

    first

--- index.js
+++ index.js
@@ -35,3 +35,39 @@ function toggleBlock2(){
     }, 10); 
   }
 }
+const arrow3 = document.getElementById('arrow3');
+arrow3.addEventListener('click', () => {
+  arrow3.classList.toggle('rotated');
+});
+function toggleBlock3(){
+  const imgBlock3 = document.getElementById('ImgBlock3');
+  if (imgBlock3.classList.contains('visible')) {
+    imgBlock3.classList.remove('fade_in');
+    setTimeout(() => {
+      imgBlock3.classList.remove('visible');
+    }, 500);
+  } else {
+    imgBlock3.classList.add('visible');
+    setTimeout(() => {
+      imgBlock3.classList.add('fade_in');
+    }, 10); 
+  }
+}
+const arrow4 = document.getElementById('arrow4');
+arrow4.addEventListener('click', () => {
+  arrow4.classList.toggle('rotated');
+});
+function toggleBlock4(){
+  const imgBlock4 = document.getElementById('ImgBlock4');
+  if (imgBlock4.classList.contains('visible')) {
+    imgBlock4.classList.remove('fade_in');
+    setTimeout(() => {
+      imgBlock4.classList.remove('visible');
+    }, 500);
+  } else {
+    imgBlock4.classList.add('visible');
+    setTimeout(() => {
+      imgBlock4.classList.add('fade_in');
+    }, 10); 
+  }
+}
